7584658d154b0fc33046aa7f3483e11880115f9d
Yudelkaa <yudelka_guillen@ucne.edu.do>
2024-09-18 21:11:02 -0400

    Agrega la entidad

---
+++ app/src/main/java/edu/ucne/yudelkaguillen_p1_ap2/data/local/entity/AlgoEntity.kt
@@ -0,0 +1,12 @@
+package edu.ucne.yudelkaguillen_p1_ap2.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Algos")
+data class AlgoEntity {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+
+
+}
