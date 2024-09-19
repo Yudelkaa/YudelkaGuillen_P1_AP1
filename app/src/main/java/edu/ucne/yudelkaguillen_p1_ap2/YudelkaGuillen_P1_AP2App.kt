3b1099962bf3d0d097af13e5e07d63592327b968
Yudelkaa <yudelka_guillen@ucne.edu.do>
2024-09-18 20:07:55 -0400

    Agrega Hilt

---
+++ app/src/main/java/edu/ucne/yudelkaguillen_p1_ap2/YudelkaGuillen_P1_AP2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.yudelkaguillen_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YudelkaGuillen_P1_AP2App : Application() {
+
+}
